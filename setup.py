29fc9ff65c7d785c3b750ed6038310958d06920c
TKperson <TKperson@users.noreply.github.com>
2020-08-20 11:53:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,17 @@ from distutils.core import setup
 
 setup(name='Discord Nuke Bot',
       version=1.0,
-      install_requires=['',
-                        'sys',
+      install_requires=['sys',
                         'json',
                         'requests',
                         're',
                         'asyncio',
                         'discord',
-                        'discord.ext',
                         'random',
                         'io',
                         'math',
                         'colorama',
-                        'lazyme.string'
+                        'lazyme'
                         
                         ],
       description='''A disord bot made in python that Nukes/Raids bots. It has 30+ commands and an active dev team it is being updated constintly 
